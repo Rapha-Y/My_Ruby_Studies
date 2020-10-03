7ff4334260eb855b94d3fc0551bc9703b2ffa91e
Rapha-Y <raph.yuki@gmail.com>
2020-10-03 14:42:37 -0300

    added user input examples

--- hello.rb
+++ hello.rb
@@ -1,13 +1,3 @@
-number_var = 63
-puts("Rule " + number_var.to_s)
-
-neg_num = -34
-puts neg_num.abs().to_s
-
-pi_num = 3.14
-puts pi_num.round()
-puts pi_num.floor()
-puts pi_num.ceil()
-
-puts Math.sqrt(49)
-puts Math.log(1)
+puts "Enter a common synonym of the word planet: "
+name = gets.chomp()
+puts ("Hello " + name + "!")
