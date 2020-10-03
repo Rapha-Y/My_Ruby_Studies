368882a841c18b6bfb4151eb9d39d77607b7ce19
Rapha-Y <raph.yuki@gmail.com>
2020-10-03 14:30:01 -0300

    added string method examples

--- hello.rb
+++ hello.rb
@@ -1,3 +1,9 @@
-greeting = "Hello"
-name = "World"
-print(greeting + " " + name)
+string_var = "    Stringy Cheese    "
+puts string_var.upcase()
+puts string_var.downcase()
+puts string_var.strip()
+puts string_var.length()
+puts string_var.include? "Stringy"
+puts string_var[4]
+puts string_var[4, 14]
+puts string_var.index("C")
