479bbefacbf876e26b39929baea1703abf1319a6
Rapha-Y <raph.yuki@gmail.com>
2020-10-05 10:50:58 -0300

    added array examplse

--- hello.rb
+++ hello.rb
@@ -1,3 +1,14 @@
-puts "Enter a common synonym of the word planet: "
-name = gets.chomp()
-puts ("Hello " + name + "!")
+characters = Array["Rose", "Lily", "Luna", "Aria"]
+puts characters[1, 2]
+
+food = Array.new
+food[food.length] = "Potato"
+food[food.length] = "Asparagus"
+food[food.length] = "Rice"
+food[food.length] = "Seaweed"
+food[food.length] = "Apple"
+
+puts "Potato is a food: " + (food.include? "Potato").to_s
+
+puts "Full food, reverse-alphabetic order: "
+puts food.sort.reverse
