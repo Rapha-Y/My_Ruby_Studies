dd1a2731155815d7085634a9383589dd920c5bf1
Rapha-Y <raph.yuki@gmail.com>
2020-10-06 16:52:59 -0300

    added guessing game

--- hello.rb
+++ hello.rb
@@ -1,6 +1,15 @@
-i = 1
+number = 0
+answer = 63
 
-while (i <= 4)
-    puts i
-    i += 1
+puts "Guess number: "
+
+while number != answer
+    number = gets.chomp().to_i
+    if number < answer
+        puts "Higher, try again: "
+    elsif number > answer
+        puts "Lower, try again: "
+    end
 end
+
+puts "You won!"
